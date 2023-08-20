0d0194fc610136c07e4e143ac63e106e28a934c3
DESKTOP-O542CIE\ragerage <parishruttemp2@gmail.com>
2023-08-20 16:35:28 +0530

    Remapped terminal mode to normal mode jump

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
 vim.cmd([[
+let mapleader="\<Space>"
+]])
+
+vim.cmd([[
 call plug#begin()
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
-Plug 'preservim/nerdtree'
 Plug 'Xuyuanp/nerdtree-git-plugin'
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 Plug 'neovim/nvim-lspconfig'
@@ -35,6 +38,7 @@ set modelines=0
 set foldmethod=expr
 set foldexpr=nvim_treesitter#foldexpr()
 set signcolumn=yes:1
+tnoremap <C-[> <C-\><C-n>
 ]])
 
 
@@ -50,7 +54,7 @@ endif
 
 
 vim.cmd([[
-nnoremap <leader>e :NERDTreeFocus<CR>
+nnoremap <leader>e :Explore<CR>
 ]])
 
 
@@ -179,6 +183,47 @@ require'lspconfig'.pyright.setup{
 
 require'lspconfig'.lua_ls.setup{
 	capabilities = capabilities;
+}
+
+require'lspconfig'.omnisharp.setup{
+  capabilities = capabilities;
+-- IMPORTANT: cmd should point to omnisharp executable
+    cmd = { "D:\\Software\\languageServers\\omnisharp-win-x64\\OmniSharp.exe" },
+
+    -- Enables support for reading code style, naming convention and analyzer
+    -- settings from .editorconfig.
+    enable_editorconfig_support = true,
+
+    -- If true, MSBuild project system will only load projects for files that
+    -- were opened in the editor. This setting is useful for big C# codebases
+    -- and allows for faster initialization of code navigation features only
+    -- for projects that are relevant to code that is being edited. With this
+    -- setting enabled OmniSharp may load fewer projects and may thus display
+    -- incomplete reference lists for symbols.
+    enable_ms_build_load_projects_on_demand = false,
+
+    -- Enables support for roslyn analyzers, code fixes and rulesets.
+    enable_roslyn_analyzers = false,
+
+    -- Specifies whether 'using' directives should be grouped and sorted during
+    -- document formatting.
+    organize_imports_on_format = false,
+
+    -- Enables support for showing unimported types and unimported extension
+    -- methods in completion lists. When committed, the appropriate using
+    -- directive will be added at the top of the current file. This option can
+    -- have a negative impact on initial completion responsiveness,
+    -- particularly for the first few completion sessions after opening a
+    -- solution.
+    enable_import_completion = false,
+
+    -- Specifies whether to include preview versions of the .NET SDK when
+    -- determining which version to use for project loading.
+    sdk_include_prereleases = true,
+
+    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+    -- true
+    analyze_open_documents_only = false,
 }
 
 -- <- LSP & completion
